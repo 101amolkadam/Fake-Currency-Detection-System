2a1417312a886682a4208f486a67b7b38065f521
101amolkadam <101amolkadam@gmail.com>
2026-04-14 20:41:55 +0530

    base url changes

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,6 +3,7 @@ import type { AnalysisResult, HistoryResponse } from "@/types";
 
 const api = axios.create({
   baseURL: "http://127.0.0.1:8000/api/v1",
+  // baseURL: "https://validcash.duckdns.org/api/v1",
   headers: { "Content-Type": "application/json" },
   timeout: 60000,
 });
